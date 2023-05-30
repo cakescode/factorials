876c85ff713c871d538f7edab9541f58a4fa6f04
cake <79643779+mxratt@users.noreply.github.com>
2023-05-29 20:52:35 -0400

    factorial.py 1.02
    made a new function that...isnt a factorial sequence....idk what it is but it creates MASSIVE numbers from very small numbers... its really fun

--- factorial.py
+++ factorial.py
@@ -42,14 +42,24 @@ def file_read(): # func to read file and use contents as input for reverse_facto
     return y
 
 def reverse_factorial(): # func to find source for a factorial using brute force
-    file_check()
-    y = file_read()
+    file_check() # checks if factorial.txt (used to input factorial) exists, if not its created
+    y = file_read() # returns input from factorial.txt as y
     for x in range(loop_limit): # loop 1 to loop_limit or until factorial source is found
         if factorial(x) == int(y):
             print(f'{x}! = {y}') # print factorial source
             exit()
-    # if loop_limit is reached and no source is found, prints the following
+    # if loop_limit is reached and no source is found, prints the following:
     print('could not find factorial source! please expand search radius (loop_limit)')
 
-loop_factorial()
+def max_factorial(x, y=0): # what have i done.....
+    if y == x:
+        return x
+    else:
+        return (x * max_factorial(x, y + 1)) ** x
+
+# run only one or the other:
+
+print(max_factorial(int(input('input value to break your computer: '))))
+# print(factorial(int(input('input value to factor: '))))
+# loop_factorial()
 # reverse_factorial()
